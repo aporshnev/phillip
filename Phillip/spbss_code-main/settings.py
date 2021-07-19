36ed8ef2d437fd3181a795e320b8070c361df7c1
aporshnev <aporshnev@gmail.com>
2021-07-19 14:48:43 +0300

    first installation

--- Phillip/spbss_code-main/settings.py
+++ Phillip/spbss_code-main/settings.py
@@ -92,3 +92,16 @@ ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 DEMO_PAGE_INTRO_HTML = """ """
 
 SECRET_KEY = '6286421661163'
+
+ROOMS = [
+    dict(
+        name='econ101',
+        display_name='Econ 101 class',
+        participant_label_file='_rooms/econ101.txt',
+        use_secure_urls=True
+    ),
+    dict(
+        name='econ_lab',
+        display_name='Experimental Economics Lab'
+    ),
+]
